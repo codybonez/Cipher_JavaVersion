41188c9fd4f08abe74f2606c1e72118491b43bfd
cody norwood <cody.norwood@student.centralia.edu>
2026-04-15 11:43:29 -0700

    Bug: Needs filtering only letters

--- src/main/Classes.java
+++ src/main/Classes.java
@@ -2,21 +2,26 @@ package main;
 
 public class Classes {
 	public static void main(String[] args) {
-	Cipher(2, "abc");	
+	
+		System.out.println(Cipher(2, "abc"));	
 	}
-  static public  String Cipher(int key, String message) {
+   public static String Cipher(int key, String message) {
 		key = (key % 26);
 		if (key < 0) key += 26;
+		
+		char c;
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
-			char[] c = message.toCharArray();
+			c = message.charAt(i);
 			
-			if (Character.isLetter(c[i])) {
-				char newChar = (char)(c[i] - key);
+			if (Character.isLetter(c)) {
+				char newChar = (char)((c + key)+ 26);
+				c = newChar;
 			}
 			else {
-			c[i] += 26;
+			c += 26;
 			}
+			builder.append(c);
 		}
 		return builder.toString();
 	}
