ab7ed293e663b0e3f29509053283c04474151d2a
cody norwood <cody.norwood@student.centralia.edu>
2026-04-20 10:08:18 -0700

    Finished algorithm

--- src/main/Classes.java
+++ src/main/Classes.java
@@ -8,20 +8,24 @@ public class Classes {
    public static String Cipher(int key, String message) {
 		key = (key % 26);
 		if (key < 0) key += 26;
-		
+		System.out.println(key);
 		char c;
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
 			c = message.charAt(i);
 			
 			if (Character.isLetter(c)) {
-				char newChar = (char)((c + key)+ 26);
-				c = newChar;
+				
+				char newChar = Character.isUpperCase(c)? 'A' : 'a';
+				System.out.println(newChar);
+				char shifted =  (char)((((c - newChar) + key) % 26) + newChar);
+				System.out.println(shifted);
+				builder.append(shifted);
 			}
 			else {
-			c += 26;
+				builder.append(c);
 			}
-			builder.append(c);
+		
 		}
 		return builder.toString();
 	}
